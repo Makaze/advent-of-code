a12fa0083cf46714573d3b321152f9369dc84534
Makaze <christopherslane.work@gmail.com>
2023-12-08 15:01:58 -0500

    Solved 8 part 2

--- 2023/8/advent8b.py
+++ 2023/8/advent8b.py
@@ -15,7 +15,7 @@ def main():
     nodes = { sep[0]: (sep[1], sep[2]) for sep in list(map(lambda x: re.split(r"\W+", x), s[2:])) }
     
     next = [A for A in nodes.keys() if A.endswith("A")]
-    Zs = [None for A in next]
+    Zs = []
     
     steps = 0
     
@@ -25,18 +25,20 @@ def main():
         these_nodes = {key: val for key, val in nodes.items() if key in next}
         step = instructs[steps % len(instructs)]
         
+        for key in these_nodes.copy():
+            if key.endswith("Z"):
+                Zs.append(steps)
+                
+                print(f"{Zs=}, {next=}, {these_nodes=} {step=}")
+                
+                these_nodes.pop(key, None)
+        
         next = [n for n in list(map(lambda x: x[0] if step == "L" else x[1], these_nodes.values()))]
         
         steps += 1
-        end = True if all(n.endswith("Z") for n in next) else False
+        end = True if all(n.endswith("Z") for n in these_nodes.keys()) else False
         
-        for i in range(len(Zs)):
-            if next[i].endswith("Z") and not Zs[i]:
-                Zs[i] = steps
-                
-                print(f"{Zs=}, {next=}, {these_nodes=} {step=}")
-        
-        if all(Zs):
+        if not these_nodes:
             LCM = math.lcm(*Zs)
             
             steps = LCM
