f8aaeb8c4dc4d3878def1cebcb049f3e8bd6bc64
Makaze <christopherslane.work@gmail.com>
2023-12-09 23:05:08 -0500

    Day 9 solution < 30 mins after start. Let's gooooo

---
+++ 2023/9/advent9.py
@@ -0,0 +1,29 @@
+# Advent of Code 2023-12-09
+# @makaze
+
+with open("data.txt") as f:
+    lines = f.read().split("\n")
+    
+lines = [list(map(int, i)) for i in list(map(str.split, lines))]
+
+p1_nexts = []
+p2_nexts = []
+
+for line in lines:
+    triangle = []
+    while any(line):
+        triangle.append(line)
+        line = [line[i] - line[i-1] for i in range(len(line)) if i > 0]
+        
+    p1_next = p2_next = 0
+    triangle.reverse()
+    
+    for line in triangle:
+        p1_next += line[-1]
+        p2_next = line[0] - p2_next
+    
+    p1_nexts.append(p1_next)
+    p2_nexts.append(p2_next)
+    
+print("Part 1:", sum(p1_nexts))
+print("Part 2:", sum(p2_nexts))
--- template.py
+++ template.py
@@ -5,7 +5,15 @@ def main():
     #with open("test.txt") as f:
     with open("data.txt") as f:
         s = f.read()
-        
+
+
+def p1():
+    pass
+
+
+def p2():
+    pass
+
 
 if __name__ == "__main__":
     main()
