c23e72a63f8f97017cb03452e6b80aa34a293d8c
Makaze <christopherslane.work@gmail.com>
2023-12-11 16:05:22 -0500

    day 11 part 1

--- template.py
+++ template.py
@@ -4,14 +4,17 @@
 def main():
     #with open("test.txt") as f:
     with open("data.txt") as f:
-        s = f.read()
+        s = f.read().split("\n")
+
+    print(f"Part 1:", p1(s))
+    print(f"Part 2:", p2(s))
 
 
-def p1():
+def p1(s):
     pass
 
 
-def p2():
+def p2(s):
     pass
 
 
