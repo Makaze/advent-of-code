785966e1c128b21d3849417285874e9fd006d945
Makaze <christopherslane.work@gmail.com>
2023-12-12 01:18:27 -0500

    Cleaning and formatting

--- 2023/10/advent10.py
+++ 2023/10/advent10.py
@@ -6,7 +6,7 @@ from enum import IntFlag
 
 colorama.init()
 
-DIR = IntFlag('Dir', ['NORTH', 'SOUTH', 'EAST', 'WEST'])
+DIR = IntFlag("Dir", ["NORTH", "SOUTH", "EAST", "WEST"])
 PIPES = {
     "|": DIR.NORTH | DIR.SOUTH,
     "-": DIR.EAST | DIR.WEST,
@@ -18,44 +18,35 @@ PIPES = {
     "S": DIR.NORTH | DIR.SOUTH | DIR.EAST | DIR.WEST,
 }
 CHARS = {val: key for key, val in PIPES.items()}
-MOVES = {
-    DIR.NORTH: (-1, 0), 
-    DIR.SOUTH: (1, 0),
-    DIR.EAST: (0, 1),
-    DIR.WEST: (0, -1)
-}
+MOVES = {DIR.NORTH: (-1, 0), DIR.SOUTH: (1, 0), DIR.EAST: (0, 1), DIR.WEST: (0, -1)}
 OPPOSITES = {
-    DIR.NORTH: DIR.SOUTH, 
+    DIR.NORTH: DIR.SOUTH,
     DIR.SOUTH: DIR.NORTH,
     DIR.EAST: DIR.WEST,
-    DIR.WEST: DIR.EAST
+    DIR.WEST: DIR.EAST,
 }
 
 explored = set()
 start = ()
-allowed_pipes = ['|']
-allowed_pipes = [
-    '|',
-    'F', '7',
-    'L', 'J',
-    'S'
-]
+allowed_pipes = ["|"]
+allowed_pipes = ["|", "F", "7", "L", "J", "S"]
+
 
 def main():
     global outside
     # with open("test.txt") as f:
     with open("data.txt") as f:
         s = f.read().split("\n")
-    
+
     print(f"Part 1:", p1(s))
     print(f"Part 2:", p2(s))
-            
+
 
 def p1(s):
     global start
     global explored
     global start_char
-    
+
     lines = []
     max_distance = 0
     for i, line in enumerate(s):
@@ -63,28 +54,28 @@ def p1(s):
             start = (i, line.index("S"))
             explored.add(start)
         lines.append(list(map(lambda x: PIPES[x], line)))
-    
-    end, pos, distance, last_move = False, start, 0, (0,0)
-    
+
+    end, pos, distance, last_move = False, start, 0, (0, 0)
+
     while end != start:
         curr_pipe = lines[pos[0]][pos[1]]
         explored.add(pos)
-        
+
         for dir, move in MOVES.items():
             next_pos = tuple(map(sum, zip(move, pos)))
-            
-            if dir & curr_pipe == 0: # Pipe doesn't go that direction
+
+            if dir & curr_pipe == 0:  # Pipe doesn't go that direction
                 continue
-            if not (0 <= next_pos[0] < len(lines)): # Not in y range
+            if not (0 <= next_pos[0] < len(lines)):  # Not in y range
                 continue
-            if not (0 <= next_pos[1] < len(lines[0])): # Not in x range
+            if not (0 <= next_pos[1] < len(lines[0])):  # Not in x range
                 continue
-            
+
             next_pipe = lines[next_pos[0]][next_pos[1]]
-            
-            if tuple(map(sum, zip(move, last_move))) == (0, 0): # Going backwards
+
+            if tuple(map(sum, zip(move, last_move))) == (0, 0):  # Going backwards
                 continue
-            if OPPOSITES[dir] & next_pipe == 0: # Next pipe doesn't attach
+            if OPPOSITES[dir] & next_pipe == 0:  # Next pipe doesn't attach
                 continue
             if next_pos == start and distance > 0:
                 end, distance = start, distance + 1
@@ -94,21 +85,26 @@ def p1(s):
             else:
                 pos, distance, last_move = next_pos, distance + 1, move
                 break
-        
+
     if end == start:
         max_distance = int((distance + 1) / 2)
-    
+
     return max_distance
+
 
 def highlight(s, sel):
     y, x = sel
     lit = s.copy()
-    lit[y] = lit[y][:x-(1 if x else 0)] + \
-        colorama.Fore.RED + lit[y][x] + colorama.Fore.RESET + \
-            lit[y][(x if x else x+1):]
-    
+    lit[y] = (
+        lit[y][: x - (1 if x else 0)]
+        + colorama.Fore.RED
+        + lit[y][x]
+        + colorama.Fore.RESET
+        + lit[y][(x if x else x + 1) :]
+    )
+
     print()
-    
+
     for line in lit:
         print(line)
 
@@ -116,16 +112,16 @@ def highlight(s, sel):
 def p2(s):
     global explored
     global start
-    
+
     inside = set()
     last = None
-    
+
     for y, line in enumerate(s):
         pipe_count = 0
         for x, char in enumerate(line):
             point = (y, x)
             line = list(line)
-            
+
             if point in explored:
                 line[x] = colorama.Fore.GREEN + line[x] + colorama.Fore.RESET
                 if char in allowed_pipes:
@@ -133,17 +129,23 @@ def p2(s):
                         pipe_count += 1
                         last = DIR.NORTH if PIPES[char] & DIR.NORTH else DIR.SOUTH
                 continue
-            
+
             if pipe_count % 2 != 0:
                 inside.add(point)
-                line[x] = colorama.Fore.RED + '\033[1m' + 'I' + '\033[0m' + colorama.Fore.RESET
+                line[x] = (
+                    colorama.Fore.RED
+                    + "\033[1m"
+                    + "I"
+                    + "\033[0m"
+                    + colorama.Fore.RESET
+                )
             else:
-                line[x] = 'O'
-            
+                line[x] = "O"
+
         print("".join(line))
-                
+
     return len(inside)
-                
+
 
 if __name__ == "__main__":
     main()
--- 2023/11/advent11.py
+++ 2023/11/advent11.py
@@ -1,5 +1,6 @@
-# Advent of Code 2023-12-
+# Advent of Code 2023-12-11
 # @makaze
+
 
 def main():
     # with open("test.txt") as f:
@@ -9,26 +10,40 @@ def main():
     print(f"Part 1:", solve(s, 2))
     print(f"Part 2:", solve(s, 1000000))
 
-def transpose(lst: iter) -> list:
-    return list(map(list, zip(*lst)))
+
+# Keeping this here for proof of concept & memory
+_shift = (
+    lambda l, s, M: (s[-1] + M if not s.append(s[-1] + M) else None)
+    if all(c == "." for c in l)
+    else s[-1]
+)
+
 
 def shift(line, shifted, multiplier):
-    if all(c == '.' for c in line):
+    if all(c == "." for c in line):
         shifted.append(shifted[-1] + multiplier)
     return shifted[-1]
 
-# Keeping this here for proof of concept & memory
-_shift = lambda l, s, M: (s[-1] + M if not s.append(s[-1] + M) else None) if all(c == '.' for c in l) else s[-1]
+
+def transpose(lst: iter) -> list:
+    return list(map(list, zip(*lst)))
+
 
 def solve(lines, multiplier):
     y_shift, x_shift, multiplier = [0], [0], multiplier - 1
     _y = [shift(line, y_shift, multiplier) for line in lines]
-    _x = [shift(line, x_shift, multiplier) for line in transpose(lines)] # Transposed
-    galaxies = [(_y[y] + y, _x[x] + x) for y, line in enumerate(lines) for x, char in enumerate(line) if char == '#']
+    _x = [shift(line, x_shift, multiplier) for line in transpose(lines)]  # Transposed
+    galaxies = [
+        (_y[y] + y, _x[x] + x)
+        for y, line in enumerate(lines)
+        for x, char in enumerate(line)
+        if char == "#"
+    ]
     pairs = {(galaxies[a], galaxies[b]) for a in range(len(galaxies)) for b in range(a)}
     distances = [(abs(a[0] - b[0]) + abs(a[1] - b[1])) for a, b in pairs]
 
     return sum(distances)
+
 
 if __name__ == "__main__":
     main()
