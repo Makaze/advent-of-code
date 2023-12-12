b30e33929726778188c30c848b42c685242adc0d
Makaze <christopherslane.work@gmail.com>
2023-12-12 01:00:35 -0500

    cleaned up day 11

--- 2023/11/advent11.py
+++ 2023/11/advent11.py
@@ -12,11 +12,11 @@ def main():
 def transpose(lst: iter) -> list:
     return list(map(list, zip(*lst)))
 
-def solve(lines, M):
-    y_shift, x_shift, M = [0], [0], M-1
-    shift = lambda l, coord, s, M: (s[-1] + M if not s.append(s[-1] + M) else coord) if all(c == '.' for c in l) else s[-1]
-    _y = [shift(line, y, y_shift, M) for y, line in enumerate(lines)]
-    _x = [shift(line, x, x_shift, M) for x, line in enumerate(transpose(lines))] # Transposed
+def solve(lines, multiplier):
+    y_shift, x_shift, multiplier = [0], [0], multiplier - 1
+    shift = lambda line, shifted, multiplier: (shifted[-1] + multiplier if not shifted.append(shifted[-1] + multiplier) else None) if all(c == '.' for c in line) else shifted[-1]
+    _y = [shift(line, y_shift, multiplier) for line in lines]
+    _x = [shift(line, x_shift, multiplier) for line in transpose(lines)] # Transposed
     galaxies = [(_y[y] + y, _x[x] + x) for y, line in enumerate(lines) for x, char in enumerate(line) if char == '#']
     pairs = {(galaxies[a], galaxies[b]) for a in range(len(galaxies)) for b in range(a)}
     distances = [(abs(a[0] - b[0]) + abs(a[1] - b[1])) for a, b in pairs]
