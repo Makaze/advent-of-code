29bd7ecfd13b75f62149f6b1340d2ccc5f4a9425
Makaze <christopherslane.work@gmail.com>
2024-12-09 20:05:05 -0500

    Current iter

--- 2024/elixir/day-05/solution.exs
+++ 2024/elixir/day-05/solution.exs
@@ -3,7 +3,7 @@ Mix.install([
 ])
 
 defmodule Rule do
-  defstruct [:left, :right]
+  defstruct [:left, :right, :sorter]
 end
 
 defmodule Solver do
@@ -13,7 +13,27 @@ defmodule Solver do
 
   import NimbleParsec
 
-  def rule_from([left, right]), do: %Rule{left: left, right: right}
+  def rule_from([left, right]) do
+    %Rule{
+      left: left,
+      right: right,
+      sorter: fn
+        ^left = l, ^right = r ->
+          IO.inspect({l, r}, label: "Already in right order")
+          true
+
+        ^right = r, ^left = l ->
+          IO.inspect({l, r}, label: "Need swapped")
+          false
+
+        l, r ->
+          IO.inspect({l, r}, label: "Not relevant")
+          true
+      end
+    }
+  end
+
+  def row_from(row), do: row
 
   anything = ignore(utf8_char([]))
 
@@ -25,10 +45,9 @@ defmodule Solver do
     |> reduce({__MODULE__, :rule_from, []})
 
   page =
-    choice([
-      integer(max: 2),
-      ignore(string(","))
-    ])
+    integer(max: 2)
+    |> repeat(ignore(string(",")) |> concat(integer(max: 2)))
+    |> reduce({__MODULE__, :row_from, []})
 
   defparsec(
     :rules,
@@ -36,7 +55,7 @@ defmodule Solver do
   )
 
   defparsec(
-    :pages,
+    :updates,
     repeat(
       choice([
         page,
@@ -45,6 +64,35 @@ defmodule Solver do
     )
   )
 
+  def rules_to_parsec([_ | _] = list) when is_list(list) do
+    list
+    |> Enum.reduce(%{}, fn %Rule{left: val, right: key}, acc ->
+      Map.update(acc, key, MapSet.new([val]), fn rest -> MapSet.put(rest, val) end)
+    end)
+  end
+
+  def sorted?(list, map, prev \\ MapSet.new([]))
+
+  def sorted?([], _, _), do: true
+
+  def sorted?([head | rest], %{} = map, prev) do
+    set = Map.get(map, head)
+
+    case set do
+      nil ->
+        sorted?(rest, map, MapSet.put(prev, head))
+
+      _ ->
+        set |> Enum.all?(fn x -> MapSet.member?(prev, x) or not Enum.member?(rest, x) end) and
+          sorted?(rest, map, MapSet.put(prev, head))
+    end
+  end
+
+  def middle([_ | _] = list) do
+    mid = div(length(list), 2)
+    Enum.at(list, mid)
+  end
+
   def sum({:ok, results, _, _, _, _}) do
     Enum.sum(results)
   end
@@ -52,14 +100,39 @@ end
 
 [rules, updates] =
   File.read!("test.txt")
+  # File.read!("input.txt")
   |> String.split(~r/\s\s+/, trim: true)
 
-# File.read!("input.txt")
+{:ok, rules, _, _, _, _} = rules |> Solver.rules()
+{:ok, updates, _, _, _, _} = updates |> Solver.updates()
+map = Solver.rules_to_parsec(rules)
 
-# File.read!("test.txt")
+part1 =
+  updates
+  |> Enum.filter(fn x -> Solver.sorted?(x, map) end)
+  |> Enum.map(&Solver.middle/1)
+  |> Enum.sum()
 
-part1 = rules |> Solver.rules()
-part2 = updates |> Solver.pages()
+part2 =
+  updates
+  |> Enum.map(fn x ->
+    sorters =
+      rules
+      |> Enum.map(fn rule ->
+        rule.sorter
+      end)
+
+    Enum.sort(x, fn a, b ->
+      Enum.all?(
+        Enum.map(sorters, fn x ->
+          x.(a, b)
+        end)
+      )
+    end)
+  end)
+
+# |> Enum.map(&Solver.middle/1)
+# |> Enum.sum()
 
 IO.inspect(part1, label: "Part 1")
 IO.inspect(part2, label: "Part 2")
