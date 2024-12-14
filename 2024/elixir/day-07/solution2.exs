a2357908673b990cab8a194871fb66f4d02ecb4a
Makaze <christopherslane.work@gmail.com>
2024-12-13 21:23:29 -0500

    Optimized solution using reverse operations

--- 2024/elixir/day-07/solution2.exs
+++ 2024/elixir/day-07/solution2.exs
@@ -9,7 +9,7 @@ defmodule Solver do
 
   import NimbleParsec
 
-  def equation_from([result | rest]), do: %{result: result, values: rest}
+  def equation_from([result | rest]), do: [result | Enum.reverse(rest)]
 
   anything = ignore(utf8_char([]))
 
@@ -30,39 +30,38 @@ defmodule Solver do
   )
 
   def divide(a, b) when rem(a, b) == 0, do: div(a, b)
-  def divide(a, b), do: :invalid
+  def divide(_a, _b), do: :invalid
 
   def unconcat(a, b) do
-    res = a * 10 ** trunc(:math.log10(b) + 1) + b
-    # IO.inspect({a, b, res}, label: "concat result")
-    res
-    size_b = :math.log10(x) |> floor() |> Kernel.+(1) |> trunc()
+    size_b = :math.log10(b) |> floor() |> Kernel.+(1) |> trunc()
     divisor = :math.pow(10, size_b) |> trunc()
-    case rem(number, divisor) do
-      val when val == b -> div(number, divisor)
+
+    case rem(a, divisor) do
+      val when val == b -> div(a, divisor)
       _ -> :invalid
     end
   end
 
-  def eval(acc, [], result, ops, _ops_list) do
-    value = {acc == result, {acc, result, ops}}
-    # IO.inspect(value, label: "End result?")
-    value
+  def eval(acc, _rest, result, ops, _ops_list) when acc == :invalid do
+    {false, {result, ops}}
   end
+
+  def eval(_acc, [], ops, result, _ops_list), do: {false, {result, ops}}
 
   def eval(nil, [first | rest], result, ops, ops_list) do
     eval(first, rest, result, ops, ops_list)
   end
 
-  def eval(acc, _, result, ops, _ops_list) when acc > result do
-    {false, {acc, result, ops}}
-  end
-
-  def eval(acc, [first | rest], result, ops, ops_list) when acc <= result do
+  def eval(acc, [first | rest], result, ops, ops_list) do
     Enum.reduce_while(ops_list, 0, fn op, _ ->
-      case eval(op.(acc, first), rest, result, [op | ops], ops_list) do
+      output = op.(acc, first)
+      solved = length(rest) == 1 and output == Enum.at(rest, 0)
+
+      case solved or eval(output, rest, result, [op | ops], ops_list) do
+        true ->
+          {:halt, {true, {result, [op | ops]}}}
+
         {true, val} ->
-          # IO.inspect(val, label: "Oh boy")
           {:halt, {true, val}}
 
         {false, val} ->
@@ -74,8 +73,8 @@ defmodule Solver do
   def sum({:ok, results, _, _, _, _}, ops) do
     results
     |> Enum.map(fn eq ->
-      case eval(nil, eq.values, eq.result, [], ops) do
-        {true, {acc, _, _}} -> acc
+      case eval(nil, eq, Enum.at(eq, 0), [], ops) do
+        {true, {acc, _}} -> acc
         {false, _} -> 0
       end
     end)
@@ -89,12 +88,14 @@ test_file =
 file =
   File.read!("input.txt")
 
-part1_test = test_file |> Solver.parse() |> Solver.sum([&+/2, &*/2])
-IO.inspect(part1_test, label: "Part 1 test")
-part1 = file |> Solver.parse() |> Solver.sum([&+/2, &*/2])
+part1_test = test_file |> Solver.parse() |> Solver.sum([&Solver.divide/2, &-/2])
+IO.inspect(part1_test, label: "Part 1 Test")
+part1 = file |> Solver.parse() |> Solver.sum([&Solver.divide/2, &-/2])
 IO.inspect(part1, label: "Part 1 Real")
 
-part2_test = test_file |> Solver.parse() |> Solver.sum([&Solver.concat_int/2, &*/2, &+/2])
+part2_test =
+  test_file |> Solver.parse() |> Solver.sum([&Solver.unconcat/2, &Solver.divide/2, &-/2])
+
 IO.inspect(part2_test, label: "Part 2 Test")
-part2 = file |> Solver.parse() |> Solver.sum([&Solver.concat_int/2, &*/2, &+/2])
+part2 = file |> Solver.parse() |> Solver.sum([&Solver.unconcat/2, &Solver.divide/2, &-/2])
 IO.inspect(part2, label: "Part 2 Real")
