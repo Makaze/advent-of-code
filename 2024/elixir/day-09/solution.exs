e41392f51e4f27c891c658014bab53d7e2ef80f9
Makaze <christopherslane.work@gmail.com>
2024-12-21 23:33:40 -0500

    feat: Day 9 part 2

--- 2024/elixir/day-09/solution.exs
+++ 2024/elixir/day-09/solution.exs
@@ -1,6 +1,6 @@
 defmodule Solver do
   @moduledoc """
-  https://adventofcode.com/2024/day/7
+  https://adventofcode.com/2024/day/9
   """
 
   def map({nil, _}), do: []
@@ -8,27 +8,76 @@ defmodule Solver do
   def map({file, index}) when rem(index, 2) == 0, do: List.duplicate(div(index, 2), file)
   def map({file, _}), do: List.duplicate(nil, file)
 
+  def defrag(files, [], _), do: files |> List.flatten() |> Enum.with_index()
+
+  def defrag(files, [move | rev], out) do
+    index =
+      files |> Enum.find_index(fn x -> hd(x) == nil and length(x) >= length(move) end)
+
+    move_index = files |> Enum.find_index(&(&1 == move))
+
+    cond do
+      index == nil or move_index > index ->
+        case index do
+          nil ->
+            defrag(files, rev, out)
+
+          _ ->
+            next = files |> Enum.at(index)
+
+            files =
+              files
+              |> Enum.with_index()
+              |> Enum.map(fn
+                {^move, _i} -> List.duplicate(nil, length(move))
+                {_v, ^index} -> move
+                {v, _i} -> v
+              end)
+
+            cond do
+              length(next) == length(move) ->
+                defrag(files, rev, out)
+
+              true ->
+                files =
+                  files
+                  |> List.insert_at(index + 1, List.duplicate(nil, length(next) - length(move)))
+
+                defrag(files, rev, out)
+            end
+        end
+
+      true ->
+        defrag(files, rev, out)
+    end
+  end
+
   def defrag([], _, out, _), do: out |> Enum.reverse() |> Enum.with_index()
   def defrag(_, _, out, max) when length(out) >= max, do: defrag([], [], out, max)
   def defrag([nil | _], [], out, max), do: defrag([], [], out, max)
+  def defrag([nil | files], [next | rev], out, max), do: defrag(files, rev, [next | out], max)
+  def defrag([next | files], rev, out, max), do: defrag(files, rev, [next | out], max)
 
-  def defrag([nil | files], [next | reverse], out, max),
-    do: defrag(files, reverse, [next | out], max)
-
-  def defrag([next | files], reverse, out, max), do: defrag(files, reverse, [next | out], max)
-
-  def sort(files) do
+  def part1(files) do
     files = files |> Enum.with_index() |> Enum.map(&map/1) |> List.flatten()
     reversed = files |> Enum.filter(& &1) |> Enum.reverse()
-    IO.inspect({files, reversed})
 
     defrag(files, reversed, [], length(reversed))
+    |> Enum.map(fn {val, index} -> val * index end)
+    |> Enum.sum()
   end
 
-  def sum(results) do
-    results
-    |> sort()
-    |> Enum.map(fn {val, index} -> val * index end)
+  def part2(files) do
+    files = files |> Enum.with_index() |> Enum.map(&map/1) |> Enum.filter(&(&1 != []))
+    reversed = files |> Enum.filter(&hd(&1)) |> Enum.reverse()
+
+    val = defrag(files, reversed, [])
+
+    val
+    |> Enum.map(fn
+      {nil, _index} -> 0
+      {val, index} -> val * index
+    end)
     |> Enum.sum()
   end
 end
@@ -41,12 +90,12 @@ file =
   File.stream!("input.txt", [], 1)
   |> Stream.map(fn <<b>> -> b - ?0 end)
 
-part1_test = test_file |> Solver.sum()
+part1_test = test_file |> Solver.part1()
 IO.inspect(part1_test, label: "Part 1 Test")
-part1 = file |> Solver.sum()
+part1 = file |> Solver.part1()
 IO.inspect(part1, label: "Part 1 Real")
 
-# part2_test = test_file |> Solver.parse() |> Solver.sum()
-# IO.inspect(part2_test, label: "Part 2 Test")
-# part2 = file |> Solver.parse() |> Solver.sum()
-# IO.inspect(part2, label: "Part 2 Real")
+part2_test = test_file |> Solver.part2()
+IO.inspect(part2_test, label: "Part 2 Test")
+part2 = file |> Solver.part2()
+IO.inspect(part2, label: "Part 2 Real")
