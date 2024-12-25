81a770f49c501171717e5b4141af0e886bf0085f
Makaze <christopherslane.work@gmail.com>
2024-12-24 20:51:33 -0500

    part 2 wip

--- 2024/elixir/day-23/solution.exs
+++ 2024/elixir/day-23/solution.exs
@@ -38,16 +38,21 @@ defmodule Solver do
     |> Enum.reduce(data, fn edge, acc ->
       {a, b} = edge
       reverse = {b, a}
-      acc = if Atom.to_string(a) |> String.starts_with?("t") do
-        update(acc, [:t, a], true)
-      else
-        acc
-      end
-      acc = if Atom.to_string(b) |> String.starts_with?("t") do
-        update(acc, [:t, b], true)
-      else
-        acc
-      end
+
+      acc =
+        if Atom.to_string(a) |> String.starts_with?("t") do
+          update(acc, [:t, a], true)
+        else
+          acc
+        end
+
+      acc =
+        if Atom.to_string(b) |> String.starts_with?("t") do
+          update(acc, [:t, b], true)
+        else
+          acc
+        end
+
       acc = update(acc, [:nodes, a, b], true)
       acc = update(acc, [:nodes, b, a], true)
       acc = put_in(acc[:edges][edge], true)
@@ -57,6 +62,7 @@ defmodule Solver do
 
   def pairs(data, t) do
     keys = Map.keys(data.nodes[t])
+
     for b <- keys, c <- keys, b < c do
       if edge?(data, {b, c}) do
         %{t => true, b => true, c => true}
@@ -64,6 +70,12 @@ defmodule Solver do
         nil
       end
     end
+  end
+
+  def complete(%{nodes: nodes}) do
+    skeys =
+      nodes
+      |> Map.keys()
   end
 
   def edge?(data, edge), do: get_in(data[:edges][edge])
@@ -74,7 +86,7 @@ defmodule Solver do
     |> Enum.map(fn t -> Task.async(fn -> pairs(map, t) end) end)
     |> Enum.map(fn task -> Task.await(task) end)
     |> List.flatten()
-    |> Enum.reject(& !&1)
+    |> Enum.reject(&(!&1))
     |> Enum.uniq()
     |> Enum.count()
   end
@@ -92,5 +104,6 @@ file =
 
 IO.inspect(test_file |> Solver.part1(), label: "Part 1 Test")
 IO.inspect(file |> Solver.part1(), label: "Part 1 Real")
+IO.inspect(file |> Solver.complete(), label: "Part 1 Real")
 # IO.inspect(test_file |> Solver.part2(), label: "Part 2 Test")
 # IO.inspect(file |> Solver.part2(), label: "Part 2 Real")
