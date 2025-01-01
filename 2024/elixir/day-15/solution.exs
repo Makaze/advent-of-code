616bdcaa74b0134a714ee85ddaa60054cdce35e7
Makaze <christopherslane.work@gmail.com>
2025-01-01 11:03:42 -0500

    Day 15 WIP

--- 2024/elixir/day-15/solution.exs
+++ 2024/elixir/day-15/solution.exs
@@ -9,130 +9,99 @@ defmodule Solver do
 
   import NimbleParsec
 
-  def robot_from([px, py, vx, vy]), do: %{p: %{x: px, y: py}, v: %{x: vx, y: vy}}
-  def int_from(["-", num]), do: num * -1
-  def int_from([num]), do: num
+  def row_from(chars) do
+    chars
+    |> Enum.map(fn
+      "#" -> :wall
+      "." -> nil
+      "O" -> :box
+      "@" -> :robot
+    end)
+  end
+
+  def moves_from(chars) do
+    chars
+    |> Enum.map(fn
+      "^" -> :n
+      "v" -> :s
+      "<" -> :w
+      ">" -> :e
+    end)
+  end
+
+  def map_from(data) do
+    [{moves, _} | data] = data |> Enum.with_index() |> Enum.reverse()
+
+    data =
+      data
+      |> Enum.reduce(%{}, fn {line, row}, acc ->
+        line
+        |> Enum.with_index()
+        |> Enum.reduce(acc, fn {char, col}, inner_acc ->
+          coord = [row, col]
+
+          inner_acc =
+            case char do
+              :robot -> put_in(inner_acc[:robot], coord)
+              _ -> inner_acc
+            end
+
+          put_in(inner_acc[coord], char)
+        end)
+      end)
+
+    {data, moves}
+  end
 
   anything = ignore(utf8_char([]))
-  neg_int = string("-") |> integer(min: 1) |> reduce({__MODULE__, :int_from, []})
-  pos_int = integer(min: 1) |> reduce({__MODULE__, :int_from, []})
+  whitespace = ignore(utf8_char([?\n, ?\r]))
+  wall = string("#")
+  box = string("O")
+  robot = string("@")
+  empty = string(".")
 
-  robot =
-    ignore(string("p="))
-    |> choice([neg_int, pos_int])
-    |> ignore(string(","))
-    |> choice([neg_int, pos_int])
-    |> ignore(string(" v="))
-    |> choice([neg_int, pos_int])
-    |> ignore(string(","))
-    |> choice([neg_int, pos_int])
-    |> reduce({__MODULE__, :robot_from, []})
+  grid_row =
+    choice([empty, wall, box, robot])
+    |> repeat(choice([empty, wall, box, robot]))
+    |> reduce({__MODULE__, :row_from, []})
+
+  moves =
+    choice([string("^"), string("v"), string("<"), string(">")])
+    |> repeat(choice([string("^"), string("v"), string("<"), string(">"), whitespace]))
+    |> reduce({__MODULE__, :moves_from, []})
 
   defparsec(
     :parse,
     repeat(
       choice([
-        robot,
-        anything
+        grid_row,
+        whitespace,
+        moves
       ])
     )
+    |> reduce({__MODULE__, :map_from, []})
   )
 
-  def predict(%{p: p, v: v}, time, constraints \\ %{h: 103, w: 101}) do
-    new = %{
-      x: rem(p.x + (constraints.w + v.x) * time, constraints.w),
-      y: rem(p.y + (constraints.h + v.y) * time, constraints.h)
-    }
+
+  def shift(coord, offset), do: Enum.zip(coord, offset) |> Enum.map(fn {a, b} -> a + b end)
+
+  def move(:s, coord), do: shift(coord, [1, 0])
+  def move(:n, coord), do: shift(coord, [-1, 0])
+  def move(:e, coord), do: shift(coord, [0, 1])
+  def move(:w, coord), do: shift(coord, [0, -1])
+  def move(_, coord), do: coord
+
+  def shove(%{robot: {row, col}} = data, [dir | rest]) do
+    next = move(dir, [row, col])
+    next_type = data[next]
   end
 
-  def part1({:ok, results, _, _, _, _}, time, constraints) do
-    results =
-      results
-      |> Enum.map(&predict(&1, time, constraints))
-
-    quads = %{
-      1 => %{
-        top: 0,
-        bottom: floor(constraints.h / 2) - 1,
-        left: 0,
-        right: floor(constraints.w / 2) - 1
-      },
-      2 => %{
-        top: 0,
-        bottom: floor(constraints.h / 2) - 1,
-        left: ceil(constraints.w / 2),
-        right: constraints.w - 1
-      },
-      3 => %{
-        top: ceil(constraints.h / 2),
-        bottom: constraints.h - 1,
-        left: ceil(constraints.w / 2),
-        right: constraints.w - 1
-      },
-      4 => %{
-        top: ceil(constraints.h / 2),
-        bottom: constraints.h - 1,
-        left: 0,
-        right: floor(constraints.w / 2) - 1
-      }
-    }
-
-    quads
-    |> Enum.reduce(%{}, fn {k, q}, acc ->
-      vals =
-        results
-        |> Enum.reject(fn robot ->
-          robot.x not in q.left..q.right or robot.y not in q.top..q.bottom
-        end)
-
-      put_in(acc[k], vals)
-    end)
-    |> Enum.map(fn {_k, v} -> length(v) end)
-    |> Enum.product()
+  def part1({:ok, results, _, _, _, _}) do
+    results
   end
 
-  def part2({:ok, results, _, _, _, _}, constraints) do
-    time =
-      100..10_000
-      |> Enum.reduce_while(100, fn i, acc ->
-        f =
-          results
-          |> Enum.map(&predict(&1, i, constraints))
-          |> Enum.frequencies()
-          |> Map.values()
-          |> Enum.max()
-
-        if f == 1 do
-          {:halt, i}
-        else
-          {:cont, f}
-        end
-      end)
-
-    print(
-      results
-      |> Enum.map(&predict(&1, time, constraints)),
-      constraints
-    )
-
-    time
-  end
-
-  defp print(prediction, constraints) do
-    p =
-      for y <- 0..(constraints.h - 1) do
-        for x <- 0..(constraints.w - 1) do
-          if %{x: x, y: y} in prediction do
-            "X"
-          else
-            " "
-          end
-        end
-        |> Enum.join("")
-      end
-      |> Enum.join("\n")
-
-    IO.puts(p)
+  def part2({:ok, results, _, _, _, _}) do
+    nil
   end
 end
 
@@ -142,6 +111,4 @@ test_file =
 file =
   File.read!("input.txt")
 
-IO.inspect(test_file |> Solver.parse() |> Solver.part1(100, %{h: 7, w: 11}), label: "Part 1 Test")
-IO.inspect(file |> Solver.parse() |> Solver.part1(100, %{h: 103, w: 101}), label: "Part 1 Real")
-IO.inspect(file |> Solver.parse() |> Solver.part2(%{h: 103, w: 101}), label: "Part 2 Real")
+IO.inspect(test_file |> Solver.parse() |> Solver.part1(), label: "Part 1 Test")
